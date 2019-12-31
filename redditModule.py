0efe1e9196773870014c62c89b9d059a0a317dfd
Muntazim Ali <muntazim.ali6@gmail.com>
2019-12-31 04:35:26 +0000

    add subredditExists function

--- redditModule.py
+++ redditModule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import praw, dotenv, os
+import praw, prawcore, dotenv, os
 
 dotenv.load_dotenv()
 
@@ -9,15 +9,31 @@ reddit = praw.Reddit(client_id = os.getenv("CLIENT_ID"), client_secret = os.gete
 
 def getTopPosts(sourceSubreddit = "all", timescale = "day", amount = 10):
 	"""Return a list of the specified amount of top posts over a given timescale from the subreddit."""
-	#TODO implement error handling for invalid subreddits
 	#TODO error handling for unspecified timescales (hour,day,week,month,year,all = valid)
 	#TODO set upper limit for amount of posts retrieved
-	source = reddit.subreddit(sourceSubreddit)
+	source = None
+	if subredditExists(sourceSubreddit):
+		source = reddit.subreddit(sourceSubreddit)
+	else:
+		return ("Subreddit invalid")
+
 	topPosts = []
 	for post in source.top(timescale, limit = amount):
 		topPosts.append(post.permalink)
 	
 	return topPosts
-	
+
+def subredditExists(sourceSubreddit):
+	"""Checks if a given subreddit exists"""
+	exists = True
+	try:
+		reddit.subreddits.search_by_name(sourceSubreddit, exact = True)
+	except prawcore.exceptions.NotFound:
+		exists = False
+
+	return exists
+
+
+
 
 
